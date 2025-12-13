3f5191516934cce3d807b4a47e3a427be18a33ab
Nahuel <nahuelhiriart98@gmail.com>
2025-12-12 21:22:41 -0300

    nuevo favicon y actualizar el título y pie de página a "Mercado Friki"

--- src/data/windows.js
+++ src/data/windows.js
@@ -1,7 +1,7 @@
 export const DECKS = [
   {
     id: 1,
-    name: 'VLAD',
+    name: 'Vlad',
     links: [
       { label: 'Moxfield', url: 'https://moxfield.com/collection/xBVb6bvDQUGTrNQP1WXwrw?q=eyJuYW1lIjoiIiwic2V0SWQiOiIiLCJyYXJpdHkiOiIiLCJjb25kaXRpb24iOiIiLCJnYW1lIjoiIiwiY2FyZExhbmd1YWdlSWQiOiIiLCJmaW5pc2giOiIiLCJpc0FsdGVyIjoiIiwiaXNQcm94eSI6IiIsIm5vdGVzIjoiIiwidHJhZGVCaW5kZXJJZCI6IiIsInByaWNlTWluaW11bSI6IiIsInByaWNlTWF4aW11bSI6IiIsInNvcnRDb2x1bW4iOiJjYXJkTmFtZSIsInNvcnREaXJlY3Rpb24iOiJhc2NlbmRpbmciLCJwYWdlTnVtYmVyIjoxLCJwYWdlU2l6ZSI6NTAsIm9yYWNsZSI6IiIsInR5cGVMaW5lIjoiIiwiY29sb3ItdyI6ZmFsc2UsImNvbG9yLXUiOmZhbHNlLCJjb2xvci1iIjpmYWxzZSwiY29sb3ItciI6ZmFsc2UsImNvbG9yLWciOmZhbHNlLCJjb2xvci1jIjpmYWxzZSwiY29sb3Itc2VsZWN0IjoiZXF1YWxzIiwiY29sb3JpZGVudGl0eS13IjpmYWxzZSwiY29sb3JpZGVudGl0eS11IjpmYWxzZSwiY29sb3JpZGVudGl0eS1iIjpmYWxzZSwiY29sb3JpZGVudGl0eS1yIjpmYWxzZSwiY29sb3JpZGVudGl0eS1nIjpmYWxzZSwibWFuYVZhbHVlIjoiIiwibWFuYVZhbHVlLXNlbGVjdCI6ImVxdWFscyIsIm1hbmFDb2xvciI6IiIsIm1hbmFDb2xvci1zZWxlY3QiOiJlcXVhbHMiLCJwb3dlciI6IiIsInBvd2VyLXNlbGVjdCI6ImVxdWFscyIsInRvdWdobmVzcyI6IiIsInRvdWdobmVzcy1zZWxlY3QiOiJlcXVhbHMiLCJsb3lhbHR5IjoiIiwibG95YWx0eS1zZWxlY3QiOiJlcXVhbHMiLCJhZmZpbGlhdGUiOiJjYXJka2luZ2RvbSIsInBsYXlTdHlsZSI6InBhcGVyRG9sbGFycyJ9' },
       { label: 'Archidekt', url: 'https://archidekt.com' }
@@ -23,7 +23,7 @@ export const DECKS = [
   },
   {
     id: 3,
-    name: 'Deck 3',
+    name: 'Prueba 1',
     links: [
       { label: 'Moxfield', url: 'https://www.moxfield.com' }
     ],
@@ -33,7 +33,7 @@ export const DECKS = [
   },
   {
     id: 4,
-    name: 'Deck 4',
+    name: 'Prueba 2',
     links: [
       { label: 'Moxfield', url: 'https://www.moxfield.com' }
     ],
@@ -43,7 +43,7 @@ export const DECKS = [
   },
   {
     id: 5,
-    name: 'Deck 5',
+    name: 'Prueba 3',
     links: [
       { label: 'Moxfield', url: 'https://www.moxfield.com' }
     ],
